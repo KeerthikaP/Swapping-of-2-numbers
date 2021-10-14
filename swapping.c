7a3290aa6f65ebe0708be38c6e7f264b66284ed7
KeerthikaP <92526980+KeerthikaP@users.noreply.github.com>
2021-10-14 19:15:26 +0530

    SWAPPING TWO NUMBERS
    THIS WILL SWAP 2 NUMBERS

---
+++ swapping.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+int main()
+{
+int x=100,y=200;
+printf("THE TWO NUMBERS BEFORE SWAPPING x=%d y=%d",x,y); /* this will print the numbers before swapping */
+x=x+y; /* 100+200 */
+y=x-y; /* 300-200 */
+x=x-y; /* 300-100 */
+printf("\n THE TWO NUMBERS AFTER SWAPPING x=%d y=%d",x,y);
+return 0;
+}
